8642d6c396cd49a03865a2fe461b4af49e508140
Levi Borland <borland.levi@gmail.com>
2019-08-15 20:23:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["Levi", "Caleb", "Cassidy"]
+  array = ["Levi", "Caleb", "Cassidy", "gang"]
 end
 
 def add_element_to_end_of_array(array, element)
